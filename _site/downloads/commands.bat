715f43257cbf7755212dc0b06784e04cc14d583c
bowerth <bo.werth@gmail.com>
2015-11-12 09:52:11 +0100

    change kg to tons in embodiedco2

--- _site/downloads/commands.bat
+++ _site/downloads/commands.bat
@@ -1,5 +1,2 @@
 cd c:\Users\werth_b\LocalData\Dropbox\GitHub\jekyll\oecd-icio\downloads
 pandoc --from markdown -t latex -o disclaimer.pdf disclaimer.md -V geometry:margin=1in
-
-:: https://github.com/bowerth/icioapp2015/wiki/Example-1
-:: https://github.com/bowerth/icioapp2015/wiki/Example-2
--- _site/downloads/commands.sh
+++
@@ -1,7 +0,0 @@
-cd ~/Dropbox/GitHub/jekyll/iciopage2015/downloads
-# pandoc --from markdown -t latex -o disclaimer.pdf disclaimer.md -V geometry:margin=1in
-pandoc --from markdown -t html -o disclaimer.html disclaimer.md
-rsync disclaimer.html ../_includes/disclaimer.html
-
-# https://github.com/bowerth/icioapp2015/wiki/Example-1
-# https://github.com/bowerth/icioapp2015/wiki/Example-2
